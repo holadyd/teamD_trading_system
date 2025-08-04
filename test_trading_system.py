4123ceef4e01623fa304286aa75210a2a7f862da
junhwik <wjdrhdsbrlfl@gmail.com>
2025-08-04 10:03:07 +0900

    unittest - 1

---
+++ test_trading_system.py
@@ -0,0 +1,21 @@
+import pytest
+
+def test_auto_trader_import():
+    trader_app = AutoTradingSystem()
+    assert trader_app is not None
+
+@pytest.mark.skip
+def test_auto_trader_buy_stock():
+    ...
+
+@pytest.mark.skip
+def test_auto_trader_sell_stock():
+    ...
+
+@pytest.mark.skip
+def test_auto_trader_buy_auto_system_():
+    ...
+
+@pytest.mark.skip
+def test_auto_trader_sell_auto_stock():
+    ...
